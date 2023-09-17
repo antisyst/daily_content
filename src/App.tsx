0971fd37af1bfb09cd1186ba31432eb986224511
Ramazan Azimli <80627823+antisyst@users.noreply.github.com>
2023-09-18 00:42:33 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const App: React.FC = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   const handlePasswordSubmit = (password: string) => {
-    const isCorrect = password === '1234'; 
+    const isCorrect = password === '2007'; 
 
     setIsLoading(true);
 
